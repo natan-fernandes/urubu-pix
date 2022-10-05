2d56b71b0ca49dd9e2b1a6cd1500e7a90b91ef92
Natan <natanfernandes@protonmail.com>
2022-10-05 20:50:07 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,18 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Success } from './src/pages/Success';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return (<Success/>);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Success.tsx
@@ -0,0 +1,51 @@
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import Lottie from 'lottie-react-native';
+import CheckAnimation from '../animations/check.json'
+
+export function Success() {
+  
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.success}>SUCESSO!</Text>
+      <Lottie style={styles.check} source={CheckAnimation} autoPlay loop={false} />
+      <Text style={styles.title}>TRANSFERÊNCIA REALIZADA</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text>Confirmar</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: "center",
+    backgroundColor: '#37b4aa'
+  },
+  title: {
+    fontSize: 25,
+    color: '#fff',
+    textAlign: 'center',
+    marginHorizontal: 80,
+    marginVertical: 20
+  },
+  button: {
+    fontSize: 20,
+    fontWeight: 700,
+    backgroundColor: '#fff',
+    paddingHorizontal: 50,
+    paddingVertical: 15,
+    borderRadius: 30,
+    marginTop: 80,
+    color: '#37b4aa'
+  },
+  success: {
+    fontSize: 60,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  check: {
+    height: 300,
+  }
+});
